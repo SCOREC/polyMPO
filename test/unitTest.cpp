5a86d624b31b6f4f9a8862b28a6ab85fed75ec29
gongy2 <gongy2@rpi.edu>
2023-03-16 01:15:16 -0400

    Update unitTest.cpp

--- test/unitTest.cpp
+++ test/unitTest.cpp
@@ -1,6 +1,9 @@
 
 
 #include "MPM.hpp"
+#include "mesh.hpp"
+#include "materialPoints.hpp"
+#include "utils.hpp"
 #include "assembly.hpp"
 #include "testUtils.hpp"
 
@@ -8,6 +11,33 @@
 int main() {
 
 //  test Vector2
+    auto v = polyMpmTest::Vector2();
+    v[0] = 1;
+    v[1] = 2;
+    assert(v[0] == 1);
+    assert(v[1] == 2);
+//  test Vector operator
+    auto v1 = polyMpmTest::Vector2(1,2);
+    auto v2 = polyMpmTest::Vector2(3,4);
+    auto v3 = v1 + v2;
+    assert(v3[0] == 4);
+    assert(v3[1] == 6);
+    auto v4 = v1 - v2;
+    assert(v4[0] == -2);
+    assert(v4[1] == -2);
+    auto v5 = v1 * 2;
+    assert(v5[0] == 2);
+    assert(v5[1] == 4);
+    auto v6 = -v1;
+    assert(v6[0] == -1);
+    assert(v6[1] == -2);
+    auto v7 = v1.dot(v2);
+    assert(v7 == 11);
+    auto v8 = v1.cross(v2);
+    assert(v8 == -2);
+    auto v9 = v1.magnitude();
+    assert(v9 - sqrt(5) < 1e-6);  
+
 
     return 0;
 }

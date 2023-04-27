3d8a5c18c92345766f7a745051803ed45e592043
Cameron Smith <smithc11@rpi.edu>
2023-04-27 10:40:13 -0400

    remove unused var, formatting

--- src/pmt_assembly.hpp
+++ src/pmt_assembly.hpp
@@ -11,7 +11,6 @@ DoubleView assembly(MPMesh& mpMesh){
     int numElms = mesh.getNumElements();
     int numMPs = mpMesh.MPs->getCount();
      
-    auto vtxCoords = mesh.getVtxCoords(); 
     auto elm2VtxConn = mesh.getElm2VtxConn();
     auto xp = mpMesh.MPs->getPositions();
     
@@ -22,16 +21,13 @@ DoubleView assembly(MPMesh& mpMesh){
       if(mask > 0) {
         int nVtxE = elm2VtxConn(elm,0);
         for(int i=0; i<nVtxE; i++){
-            int vID = elm2VtxConn(elm,i+1)-1;
-            auto vertexLoc = vtxCoords(vID);
-            double distance = mpPositions(pid,0);
-            Kokkos::atomic_add(&vField(vID),distance);
+          int vID = elm2VtxConn(elm,i+1)-1;
+          double distance = mpPositions(pid,0);
+          Kokkos::atomic_add(&vField(vID),distance);
         }
       }
     };
     MPs->parallel_for(assemble);
-
-   
     return vField;
 }
 

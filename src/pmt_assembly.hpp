e1e6ccef0b836fec92de480060c61518ff3e703c
Cameron Smith <smithc11@rpi.edu>
2023-04-27 00:05:45 -0400

    better var name

--- src/pmt_assembly.hpp
+++ src/pmt_assembly.hpp
@@ -18,11 +18,11 @@ DoubleView assembly(MPMesh& mpMesh){
     DoubleView vField("vField2",numVtxs);
     auto MPs = mpMesh.MPs;
     auto mpPositions = MPs->getData<0>();
-    auto assemble = PS_LAMBDA(const int& e, const int& pid, const int& mask) {
+    auto assemble = PS_LAMBDA(const int& elm, const int& pid, const int& mask) {
       if(mask > 0) {
-        int nVtxE = elm2VtxConn(e,0);
+        int nVtxE = elm2VtxConn(elm,0);
         for(int i=0; i<nVtxE; i++){
-            int vID = elm2VtxConn(e,i+1)-1;
+            int vID = elm2VtxConn(elm,i+1)-1;
             auto vertexLoc = vtxCoords(vID);
             double distance = mpPositions(pid,0);
             Kokkos::atomic_add(&vField(vID),distance);

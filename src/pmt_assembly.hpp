1090291c963b62750ec7820fa5b3bd08bdca85f8
Cameron Smith <smithc11@rpi.edu>
2023-04-27 13:32:10 -0400

    comments, pid -> mp

--- src/pmt_assembly.hpp
+++ src/pmt_assembly.hpp
@@ -12,16 +12,20 @@ DoubleView assembly(MPMesh& mpMesh){
     
     DoubleView vField("vField2",numVtxs);
     auto MPs = mpMesh.MPs;
-    auto mpPositions = MPs->getData<0>();
-    auto assemble = PS_LAMBDA(const int& elm, const int& pid, const int& mask) {
-      if(mask > 0) {
-        int nVtxE = elm2VtxConn(elm,0);
-        for(int i=0; i<nVtxE; i++){
-          int vID = elm2VtxConn(elm,i+1)-1;
-          double distance = mpPositions(pid,0);
-          Kokkos::atomic_add(&vField(vID),distance);
-        }
-      }
+    auto mpPositions = MPs->getData<0>(); //get the array of MP coordinates/positions
+    auto assemble = PS_LAMBDA(const int& elm, const int& mp, const int& mask) {
+    //for elm in elementsInMesh { //pseudo code - the 'parallel_for' handles this
+    //  for mp in materialPointsInElm { //pseudo code (cont.)
+          if(mask > 0) { //if material point is 'active'/'enabled'
+            int nVtxE = elm2VtxConn(elm,0); //number of vertices bounding the element
+            for(int i=0; i<nVtxE; i++){
+              int vID = elm2VtxConn(elm,i+1)-1; //vID = vertex id
+              double distance = mpPositions(mp,0);
+              Kokkos::atomic_add(&vField(vID),distance);
+            }
+          }
+    //  }
+    //}
     };
     MPs->parallel_for(assemble);
     return vField;

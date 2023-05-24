3dfb45137d9b3170dae007d61c1fcc1d2e1cc363
RPIFisherman <yuyanggong.rpi@gmail.com>
2023-05-24 13:55:05 -0400

    optimize the enum types

--- src/pmt_materialPoints.hpp
+++ src/pmt_materialPoints.hpp
@@ -14,12 +14,12 @@ namespace polyMpmTest{
 using particle_structs::DPS;
 using particle_structs::MemberTypes;
 
-typedef bool mp_status_t;
+//typedef bool mp_flag_t;
+typedef int mp_flag_t;
 typedef int  mp_elem_id_t;
 typedef double mp_vec2d_t[2];
 typedef double mp_vec3d_t[3];
 typedef double mp_sym_mat3d_t[6];
-typedef bool mp_flag_basis_t;
 typedef double mp_basis_t[maxVtxsPerElm];
 typedef double mp_basis_grad2d_t[maxVtxsPerElm][2];
 typedef double mp_constv_mdl_param_t[12];
@@ -28,22 +28,38 @@ enum MaterialPointSlice {
   MP_STATUS = 0,
   MP_CUR_ELM_ID = 1,
   MP_TGT_ELM_ID = 2,
-  MP_CUR_POSITION_LAT_LON = 3,
-  MP_TGT_POSITION_LAT_LON = 4,
-  MP_CUR_POSITION_XYZ = 5,
-  MP_TGT_POSITION_XYZ = 6,
+  MP_CUR_POS_LAT_LON = 3,
+  MP_TGT_POS_LAT_LON = 4,
+  MP_CUR_POS_XYZ = 5,
+  MP_TGT_POS_XYZ = 6,
   MP_FLAG_BASIS_VALS = 7,
   MP_BASIS_VALS = 8,
   MP_BASIS_GRAD_VALS = 9,
-  MP_VELOCITY = 10,
+  MP_VEL = 10,
   MP_STRAIN_RATE = 11,
   MP_STRESS = 12,
   MP_STRESS_DIV = 13,
-  SHEAR_TRACTION = 14,
-  MP_CONST_MDL_PARAM = 15
+  MP_SHEAR_TRACTION = 14,
+  MP_CONSTV_MDL_PARAM = 15
 };
 
-typedef MemberTypes<mp_elem_id_t,mp_elem_id_t,mp_elem_id_t,mp_vec2d_t,mp_vec2d_t,mp_vec3d_t,mp_vec3d_t,mp_elem_id_t,mp_basis_t,mp_basis_grad2d_t,mp_vec2d_t,mp_sym_mat3d_t,mp_sym_mat3d_t,mp_vec3d_t,mp_vec3d_t,mp_constv_mdl_param_t> MaterialPointTypes;
+typedef MemberTypes<mp_flag_t,              //MP_STATUS
+                    mp_elem_id_t,           //MP_CUR_ELM_ID
+                    mp_elem_id_t,           //MP_TGT_ELM_ID
+                    mp_vec2d_t,             //MP_CUR_POS_LAT_LON
+                    mp_vec2d_t,             //MP_TGT_POS_LAT_LON
+                    mp_vec3d_t,             //MP_CUR_POS_XYZ
+                    mp_vec3d_t,             //MP_TGT_POS_XYZ
+                    mp_flag_t,              //MP_FLAG_BASIS_VALS
+                    mp_basis_t,             //MP_BASIS_VALS
+                    mp_basis_grad2d_t,      //MP_BASIS_GRAD_VALS
+                    mp_vec2d_t,             //MP_VEL
+                    mp_sym_mat3d_t,         //MP_STRAIN_RATE
+                    mp_sym_mat3d_t,         //MP_STRESS
+                    mp_vec3d_t,             //MP_STRESS_DIV
+                    mp_vec3d_t,             //MP_SHEAR_TRACTION
+                    mp_constv_mdl_param_t   //MP_CONSTV_MDL_PARAM
+                    >getMemberViewgetMemberViewMaterialPointTypes;
 typedef ps::ParticleStructure<MaterialPointTypes> PS;
 
 PS* createDPS(int numElms, int numMPs, Vector2View positions, IntView mpsPerElm, IntView mp2elm);

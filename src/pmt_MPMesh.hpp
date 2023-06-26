662844b02c7a6bebba507cc99177097801f59959
RPIFisherman <yuyanggong.rpi@gmail.com>
2023-06-26 09:31:17 -0400

    implement CVT trackings(elm and edge center based) and T2L tracking

--- src/pmt_MPMesh.hpp
+++ src/pmt_MPMesh.hpp
@@ -25,9 +25,9 @@ class MPMesh{
     Mesh getMesh() { return mesh_; }
     //void setAssembly(DoubleView asmReturn) { mesh_.setAssemblyReturn(asmReturn); }
     
-    void CVTTrackingEdgeCenterBased(Vector2View dx, const int printFlag = -1);
-    void CVTTrackingElmCenterBased(Vector2View dx, const int printFlag = -1);
-    void T2LTracking(Vector2View dx, const int printFlag = -1);
+    void CVTTrackingEdgeCenterBased(Vector2View dx);
+    void CVTTrackingElmCenterBased(Vector2View dx);
+    void T2LTracking(Vector2View dx);
 };
 
 }//namespace polyMpmTest end

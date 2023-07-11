14a182d447be0f6839661e3502625fa9eb6ed22f
Cameron Smith <smithc11@rpi.edu>
2023-07-11 14:22:23 -0400

    wip: function prototype

--- src/pmo_c.h
+++ src/pmo_c.h
@@ -11,6 +11,7 @@ mpmesh polympo_createMpMesh();
 void polympo_deleteMpMesh(mpmesh mpMesh);
 void polympo_finalize();
 void polympo_setMPVelArray(mpmesh mpMeshIn, int size, double* array);
+void polympo_setMP2dVelArray(mpmesh mpMeshIn, int rank1size, int rank2size, double* array);
 void polympo_getMPVelArray(mpmesh mpMeshIn, int size, double* array);
 void polympo_setMeshVelArray(mpmesh mpMeshIn, int size, double* array);
 void polympo_getMeshVelArray(mpmesh mpMeshIn, int size, double* array);

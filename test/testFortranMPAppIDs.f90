08c91aded3232693e2759cd76700a49c2f155b5d
Angelyr <scardking@gmail.com>
2023-12-07 20:53:43 -0500

    create MP mesh

--- test/testFortranMPAppIDs.f90
+++ test/testFortranMPAppIDs.f90
@@ -4,10 +4,24 @@ end module MYDATA_QUEUE
 
 program main
     use MYDATA_QUEUE
+    use :: polympo
+    use :: iso_c_binding
+    implicit none
+    include 'mpif.h'
+
     type (QUEUE_STRUCT), pointer :: queue
     logical :: success
-    integer :: result
+    integer :: setMeshOption, setMPOption
+    integer :: ierr, self
+    integer :: mpi_comm_handle = MPI_COMM_WORLD
+    type(c_ptr) :: mpMesh
 
+    ! Initialize
+    call mpi_init(ierr)
+    call mpi_comm_rank(mpi_comm_handle, self, ierr)
+    call polympo_setMPICommunicator(mpi_comm_handle)
+    call polympo_initialize()
+    ! Create Queue
     call queue_create(queue, 5)
     call queue_append_data( queue, 122, success )
     call queue_append_data( queue, 233, success )
@@ -18,6 +32,13 @@ program main
     print *, queue_retrieve_data(queue)
     print *, queue_retrieve_data(queue)
     print *, queue_retrieve_data(queue)
-
+    ! Create Mesh
+    setMeshOption = 1 !create a hard coded planar test mesh
+    setMPOption = 0   !create an empty set of MPs
+    mpMesh = polympo_createMPMesh(setMeshOption, setMPOption)
+    ! Clean Up
+    call polympo_deleteMPMesh(mpMesh)
     call queue_destroy(queue)
+    call polympo_finalize()
+    call mpi_finalize(ierr)
 end program

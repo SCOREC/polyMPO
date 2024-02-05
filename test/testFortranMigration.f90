ec00e1807b1dda87e84bca9611d70c2fafc6c966
Angelyr <scardking@gmail.com>
2024-02-05 17:39:43 -0500

    mesh fields

--- test/testFortranMigration.f90
+++ test/testFortranMigration.f90
@@ -1,12 +1,21 @@
 program main
     use :: polympo
+    use :: readMPAS
     use :: iso_c_binding
     implicit none
     include 'mpif.h'
+
     integer :: ierr, self
     integer :: mpi_comm_handle = MPI_COMM_WORLD
     type(c_ptr) :: mpMesh
     integer :: setMeshOption, setMPOption
+    character (len=64) :: onSphere
+    integer :: maxEdges, vertexDegree, nCells, nVertices
+    real(kind=MPAS_RKIND) :: sphereRadius
+    integer, dimension(:), pointer :: nEdgesOnCell
+    real(kind=MPAS_RKIND), dimension(:), pointer :: xVertex, yVertex, zVertex
+    real(kind=MPAS_RKIND), dimension(:), pointer :: latVertex
+    integer, dimension(:,:), pointer :: verticesOnCell, cellsOnCell
 
     ! Initialize
     call mpi_init(ierr)
@@ -15,11 +24,26 @@ program main
     call polympo_initialize()
 
     ! Create Mesh
-    setMeshOption = 1 !create a hard coded planar test mesh
+    setMeshOption = 0 !create an empty mesh
     setMPOption = 0   !create an empty set of MPs
     mpMesh = polympo_createMPMesh(setMeshOption, setMPOption)
 
+    ! call loadMPASMeshInPolyMPO(mpMesh, maxEdges, vertexDegree, &
+    !                     nCells, nVertices, nEdgesOnCell, &
+    !                     onSphere, sphereRadius, &
+    !                     xVertex, yVertex, zVertex, &
+    !                     latVertex, &
+    !                     verticesOnCell, cellsOnCell)
+
+
     ! Clean Up
+    deallocate(nEdgesOnCell)
+    deallocate(xVertex)
+    deallocate(yVertex)
+    deallocate(zVertex)
+    deallocate(verticesOnCell)
+    deallocate(cellsOnCell)
+
     call polympo_deleteMPMesh(mpMesh)
     call polympo_finalize()
     call mpi_finalize(ierr)

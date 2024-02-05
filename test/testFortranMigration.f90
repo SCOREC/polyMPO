43c641224039c5d3ca9f09187af615352e88c2ac
Angelyr <scardking@gmail.com>
2024-02-05 13:41:36 -0500

    initialize empty mesh

--- test/testFortranMigration.f90
+++ test/testFortranMigration.f90
@@ -3,4 +3,24 @@ program main
     use :: iso_c_binding
     implicit none
     include 'mpif.h'
+    integer :: ierr, self
+    integer :: mpi_comm_handle = MPI_COMM_WORLD
+    type(c_ptr) :: mpMesh
+    integer :: setMeshOption, setMPOption
+
+    ! Initialize
+    call mpi_init(ierr)
+    call mpi_comm_rank(mpi_comm_handle, self, ierr)
+    call polympo_setMPICommunicator(mpi_comm_handle)
+    call polympo_initialize()
+
+    ! Create Mesh
+    setMeshOption = 1 !create a hard coded planar test mesh
+    setMPOption = 0   !create an empty set of MPs
+    mpMesh = polympo_createMPMesh(setMeshOption, setMPOption)
+
+    ! Clean Up
+    call polympo_deleteMPMesh(mpMesh)
+    call polympo_finalize()
+    call mpi_finalize(ierr)
 end program

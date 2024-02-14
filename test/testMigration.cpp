007c14e761b76d7ee7ef25e1a931900b4120f98e
Angelyr <scardking@gmail.com>
2024-02-14 15:21:55 -0500

    moved out divide procs

--- test/testMigration.cpp
+++ test/testMigration.cpp
@@ -3,6 +3,31 @@
 
 using namespace polyMPO;
 
+IntView divideProcsInHalf(Mesh* mesh, int numElms, int comm_rank, int comm_size) {
+    auto elm2VtxConn = mesh->getElm2VtxConn();
+    auto vtxCoords = mesh->getMeshField<polyMPO::MeshF_VtxCoords>(); 
+    
+    Vec3dView elmCenter("elementCenter",numElms);
+    Kokkos::parallel_for("calcElementCenter", numElms, KOKKOS_LAMBDA(const int elm){  
+        int numVtx = elm2VtxConn(elm,0);
+        double sum_x = 0.0, sum_y = 0.0, sum_z = 0.0;
+        for(int i=1; i<= numVtx; i++){
+            sum_x += vtxCoords(elm2VtxConn(elm,i)-1,0);
+            sum_y += vtxCoords(elm2VtxConn(elm,i)-1,1);
+            sum_z += vtxCoords(elm2VtxConn(elm,i)-1,2);
+        }
+        elmCenter(elm)[0] = sum_x/numVtx;
+        elmCenter(elm)[1] = sum_y/numVtx;
+        elmCenter(elm)[2] = sum_z/numVtx;
+    });
+
+    IntView owningProc("owningProc", numElms);
+    Kokkos::parallel_for("setOwningProc", numElms, KOKKOS_LAMBDA(const int elm){
+        owningProc(elm) = elmCenter(elm)[1] > 0 ? comm_size-1 : 0;
+    });
+    return owningProc;
+}
+
 int main(int argc, char* argv[] ) {
     MPI_Init(&argc, &argv);
     Kokkos::initialize(argc,argv);
@@ -36,27 +61,7 @@ int main(int argc, char* argv[] ) {
         MaterialPoints p_MPs(numElms, numMPs, mpsPerElm, activeMP2Elm, activeMPIDs);
         mpmesh->p_MPs = &p_MPs;
 
-        auto elm2VtxConn = mesh->getElm2VtxConn();
-        auto vtxCoords = mesh->getMeshField<polyMPO::MeshF_VtxCoords>(); 
-        
-        Vec3dView elmCenter("elementCenter",numElms);
-        Kokkos::parallel_for("calcElementCenter", numElms, KOKKOS_LAMBDA(const int elm){  
-            int numVtx = elm2VtxConn(elm,0);
-            double sum_x = 0.0, sum_y = 0.0, sum_z = 0.0;
-            for(int i=1; i<= numVtx; i++){
-                sum_x += vtxCoords(elm2VtxConn(elm,i)-1,0);
-                sum_y += vtxCoords(elm2VtxConn(elm,i)-1,1);
-                sum_z += vtxCoords(elm2VtxConn(elm,i)-1,2);
-            }
-            elmCenter(elm)[0] = sum_x/numVtx;
-            elmCenter(elm)[1] = sum_y/numVtx;
-            elmCenter(elm)[2] = sum_z/numVtx;
-        });
-
-        IntView owningProc("owningProc", numElms);
-        Kokkos::parallel_for("setOwningProc", numElms, KOKKOS_LAMBDA(const int elm){
-            owningProc(elm) = elmCenter(elm)[1] > 0 ? comm_size-1 : 0;
-        });
+        IntView owningProc = divideProcsInHalf(mesh, numElms, comm_rank, comm_size);
         mesh->setMeshEdit(true);
         mesh->setOwningProc(owningProc);
         mpmesh->push();

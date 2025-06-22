fadeaa13ec322055040826297762e9585614ec9b
Angelyr <scardking@gmail.com>
2025-06-21 19:36:16 -0700

    added weekly perlmutter test

--- .github/workflows/perlmutter/run.sh
+++ .github/workflows/perlmutter/run.sh
@@ -10,5 +10,5 @@ module load cray-netcdf
 export MPICH_CXX=$root/kokkos/bin/nvcc_wrapper
 
 cd build-$name
-salloc --time 00:20:00 --constrain=gpu --qos=interactive --nodes=1 --ntasks-per-node=40 --cpus-per-task=1 --gpus=1 --account=m4564 ctest
+salloc --time 00:10:00 --constrain=gpu --qos=interactive --nodes=1 --ntasks-per-node=40 --cpus-per-task=1 --gpus=1 --account=m4564 ctest
 cat $PWD/Testing/Temporary/LastTest.log
